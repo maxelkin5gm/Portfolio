cc2a121a14ee60685f6a5641dfcdca6af92064a5
Maxim Elkin <maxelkin5gm@gmail.com>
2023-03-13 16:03:56 +0300

    change about description

--- components/AboutSection/AboutSection.tsx
+++ components/AboutSection/AboutSection.tsx
@@ -13,14 +13,13 @@ const AboutSection = () => {
                     <h2 className={cl.about__title}>Hi, I&apos;m Maxim</h2>
 
                     <p className={cl.about__text}>
-                        More than 4 years diligently studying programming.
-                        <br/>
-                        I have a diploma from VyatSU &quot;Informatics and Computer Engineering&quot;, now I am studying at the master&apos;s program at BMSTU (iu7).
-                        During this time I have tested a large number of languages and technologies.
-                        I studied in depth everything related to the WEB and created several projects.
+                        I have a diploma from VyatSU &quot;Informatics and Computer Engineering&quot; and more 4 years diligently studying programming.
                         <br/>
                         <br/>
-                        I currently live in Moscow and am ready to consider job offers.
+                        During this time I have tested a large number of languages and technologies. For the past 2 years, I have been diving deeper into technologies such as java, spring, hibernate and other related ones.
+                        <br/>
+                        <br/>
+                        Now I am looking for new opportunities to grow and develop myself with a new team.
                         <br/>
                         Working with me, you get a person who quickly adapts to the project, has a good learning ability and loves his job.
                     </p>
